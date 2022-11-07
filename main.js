205fff63ea91646b2f3acdc844901ce73e207b28
Sho Maeda <syou20020911@gmail.com>
2022-11-07 21:53:31 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 const sendLine = (body) => {
-  let options = {
-    "method": "post",
-    "payload": {
-      'message': body
-    },
-    "headers": { "Authorization": "Bearer " + "XXXXXXXXXX" }
-  };
-  UrlFetchApp.fetch("https://notify-api.line.me/api/notify", options);
+  let options ={
+        "method"  : "post",
+        "payload" : {
+          'message': body
+         },
+        "headers" : {"Authorization" : "Bearer "+ "u3NB4PxYeGkJNsONAi2UIKYKHquQPy2gYRK8Xoa8cPd"}  
+      };
+    UrlFetchApp.fetch("https://notify-api.line.me/api/notify", options);
 }
 
 const setSearchCondition = () => {
-  const now = Math.floor(new Date().getTime() / 1000);
+  const now = Math.floor(new Date().getTime() / 1000) ;
   const interval = 20;
   const term = now - (60 * interval);
 
-  return '(is:unread after:' + term + ')';
+  return '(is:unread after:'+ term + ')';
 }
 
 const searchMail = (searchCondition) => {
@@ -28,15 +28,14 @@ const getMessages = (searchResult) => {
   for (const messages of mails) {
     const message = messages.pop()
     try {
-       // if you want to filtering you can use this. 
-      if (message.getFrom() !== "target@gmail.com") { throw new Error('This mail is not target.') }
+      if (message.getFrom() !== 'syou09110911@gmail.com') { throw new Error('exit;') } 
       detailMessages.push(
-        "\n【日時】: " + Utilities.formatDate(message.getDate(), 'JST', 'm月d日 HH時mm分')
+          "\n【日時】: " + Utilities.formatDate(message.getDate()　, 'JST', 'm月d日 HH時mm分')
         + "\n【送信者】: " + message.getFrom()
         + "\n【タイトル】: " + message.getSubject()
-        + "\n【内容】: \n" + message.getPlainBody().slice(0, 200)
+        + "\n【内容】&#128514; &#128514; &#128514; &#128514; \n" + message.getPlainBody().slice(0,200)
       )
-    } catch (e) {
+    } catch(e) {
       return new Error('message: ' + e)
     }
   }
@@ -46,12 +45,12 @@ const getMessages = (searchResult) => {
 }
 
 const main = () => {
-  const result = searchMail(setSearchCondition())
+  const result = searchMail(setSearchCondition()) 
   if (result.length > 0) {
     const messages = getMessages(result)
-
+ 
     messages.forEach(message => {
       sendLine(message)
-    })
+    }) 
   }
 }
